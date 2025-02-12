01ca91f602ce0ebe77bede305546a597b9c5e690
dang.vh <dang.vh2@orai.io>
2025-02-12 17:07:00 +0700

    config

--- src/server/app/services/swapContract.service.ts
+++ src/server/app/services/swapContract.service.ts
@@ -1,5 +1,19 @@
+import { CosmosRosetta, TatumSDK, Network } from "../../../service"
+
 const swap = async (sender: string, typeUrl: string, value: string) => {
   console.log(sender, typeUrl, value)
+
+  const tatumCosmos = await TatumSDK.init<CosmosRosetta>({network: Network.COSMOS_ROSETTA})
+  await tatumCosmos.simulate.setupQueryClient("https://rpc.orai.io")
+  const v = Uint8Array.from(Buffer.from(value, 'base64'))
+  const msgs = [
+    {
+      typeUrl: typeUrl,
+      value: v
+    }
+  ]
+  const res = await tatumCosmos.simulate.simulate(sender, msgs)
+  console.log(res.result?.events)
   return
 }
 

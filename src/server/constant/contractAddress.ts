c187ca1d0da9ba7333e68240d3fa9a924b8196f8
ledanghuy1811 <huygamo567@gmail.com>
2025-02-13 14:59:36 +0700

    feat: add switch case handle contract address

---
+++ src/server/constant/contractAddress.ts
@@ -0,0 +1,4 @@
+export const ORAI_CONTRACT = {
+    SWAP: "orai1yglsm0u2x3xmct9kq3lxa654cshaxj9j5d9rw5enemkkkdjgzj7sr3gwt0",
+    BRIDGE: "orai195269awwnt5m6c843q6w7hp8rt0k7syfu9de4h0wz384slshuzps8y7ccm"
+}

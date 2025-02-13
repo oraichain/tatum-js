61900947b7606be5028c4727c5920a33aa251d14
dang.vh <dang.vh2@orai.io>
2025-02-13 10:20:47 +0700

    add check with msg swap and action

--- src/bridge-demo.ts
+++ src/bridge-demo.ts
@@ -1,35 +1,87 @@
 import { toUtf8 } from '@cosmjs/encoding';
 import { CosmosRosetta, Ethereum, Network, TatumSDK } from './service'
 import { MsgUpdateAdmin, MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+import { Coin } from 'cosmjs-types/cosmos/base/v1beta1/coin';
 
+
+interface Message {
+  [key: string]: any;  // Additional dynamic fields for each message
+}
 
 const main = async () => {
   const tatumCosmos = await TatumSDK.init<CosmosRosetta>({ network: Network.COSMOS_ROSETTA })
   await tatumCosmos.simulate.setupQueryClient("https://rpc.orai.io")
-    const res = await tatumCosmos.simulate.simulate(
-      "orai1qpuundpvtymcyq3cmcty3udf2zy0m509w4kg8w",
-      [
+
+  const msgs = [
       {
         typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
         value: MsgExecuteContract.encode({
           sender: "orai1qpuundpvtymcyq3cmcty3udf2zy0m509w4kg8w",
-          contract: "orai15un8msx3n5zf9ahlxmfeqd2kwa5wm0nrpxer304m9nd5q6qq0g6sku5pdd",
+          contract: "orai1yglsm0u2x3xmct9kq3lxa654cshaxj9j5d9rw5enemkkkdjgzj7sr3gwt0",
           msg: toUtf8(
-            JSON.stringify({
-              increase_allowance: {
-                amount: "100000000000000",
-                spender:
-                  "orai10s0c75gw5y5eftms5ncfknw6lzmx0dyhedn75uz793m8zwz4g8zq4d9x9a",
-              },
-            })
+            `{
+  "swap_and_action": {
+    "affiliates": [],
+    "min_asset": {
+      "cw20": {
+        "amount": "394980",
+        "address": "orai12hzjxfh77wl572gdzct2fxv2arxcwh6gykc7qh"
+      }
+    },
+    "post_swap_action": {
+      "transfer": {
+        "to_address": "orai1qpuundpvtymcyq3cmcty3udf2zy0m509w4kg8w"
+      }
+    },
+    "timeout_timestamp": 1739560318000000000,
+    "user_swap": {
+      "swap_exact_asset_in": {
+        "swap_venue_name": "oraidex",
+        "operations": [
+          {
+            "denom_in": "orai",
+            "denom_out": "orai15un8msx3n5zf9ahlxmfeqd2kwa5wm0nrpxer304m9nd5q6qq0g6sku5pdd",
+            "pool": "orai-orai15un8msx3n5zf9ahlxmfeqd2kwa5wm0nrpxer304m9nd5q6qq0g6sku5pdd-3000000000-100"
+          },
+          {
+            "denom_in": "orai15un8msx3n5zf9ahlxmfeqd2kwa5wm0nrpxer304m9nd5q6qq0g6sku5pdd",
+            "denom_out": "orai12hzjxfh77wl572gdzct2fxv2arxcwh6gykc7qh",
+            "pool": "orai12hzjxfh77wl572gdzct2fxv2arxcwh6gykc7qh-orai15un8msx3n5zf9ahlxmfeqd2kwa5wm0nrpxer304m9nd5q6qq0g6sku5pdd-500000000-10"
+          }
+        ]
+      }
+    }
+  }
+}`
           ),
-          funds: [],
+          funds: [Coin.fromJSON({
+            denom: "orai",
+            amount: "100000",
+          })],
         }).finish(),
-      },
-    ]
+      } 
+     ]
+
+    const res = await tatumCosmos.simulate.simulate(
+      "orai1qpuundpvtymcyq3cmcty3udf2zy0m509w4kg8w",
+      msgs
     )
-  
-    console.log(res)
+
+    console.log(Buffer.from(msgs[0].value).toString('base64'))
+
+    const res1 = await tatumCosmos.ammV2.parseSwapAndAction({events: res.result!.events, message: msgs})
+    console.log(res1.data)
+
+    const ms = MsgExecuteContract.decode(msgs[0].value)
+
+    const val = (new TextDecoder).decode(ms.msg)
+    const message = JSON.parse(val)
+    // Get the name of the first element (first key)
+const firstKey = Object.keys(message)[0];
+console.log(firstKey)
+
+
+    // console.log(res)
 }
 
 main()

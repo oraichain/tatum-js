56cfec36987ff592a22f47a4eb9367ea02f4fa23
ledanghuy1811 <huygamo567@gmail.com>
2025-03-24 17:06:53 +0700

    feat: add parse create denom for pool parser

--- src/service/pool/pool.dto.ts
+++ src/service/pool/pool.dto.ts
@@ -1,1 +1,14 @@
 import { Event } from '@cosmjs/stargate'
+
+export interface CreateDenomCosmosData {
+  message: any
+  events: Event[]
+}
+
+export interface CreateDenomResponse {
+  name: string
+  symbol: string
+  baseDenom: string
+  exponent: string
+  description: string
+}
--- src/service/pool/pool.ts
+++ src/service/pool/pool.ts
@@ -1,9 +1,10 @@
-import Container, { Service } from "typedi";
+import Container, { Service } from 'typedi'
 
-import { TatumConnector } from "../../connector";
-import { CommonInfoCosmos } from "../common-info";
-import { TatumConfig } from "../tatum";
-import { CONFIG } from "../../util";
+import { TatumConnector } from '../../connector'
+import { CONFIG, ResponseDto, Status } from '../../util'
+import { CommonInfoCosmos } from '../common-info'
+import { TatumConfig } from '../tatum'
+import { CreateDenomCosmosData, CreateDenomResponse } from './pool.dto'
 
 @Service({
   factory: (data: { id: string }) => new PoolCosmos(data.id),
@@ -18,6 +19,49 @@ export class PoolCosmos {
     this.connector = Container.of(this.id).get(TatumConnector)
     this.config = Container.of(this.id).get(CONFIG)
     this.commonInfo = Container.of(this.id).get(CommonInfoCosmos)
+  }
+
+  /**
+   * Parse Create Denom msg
+   */
+  async parseCreateDenom(data: CreateDenomCosmosData): Promise<ResponseDto<CreateDenomResponse | null>> {
+    let returnData: CreateDenomResponse = {} as any
+    let error = null
+    let status = Status.SUCCESS
+
+    try {
+      const setDenomEvent = data.events.find((event) => event.type === 'set_denom_metadata')
+      if (!setDenomEvent) {
+        throw new Error('Set Denom Event not found')
+      }
+
+      for (const attribute of setDenomEvent.attributes) {
+        switch (attribute.key) {
+          case 'denom':
+            returnData.baseDenom = attribute.value
+            break
+          case 'denom_metadata':
+            const rawMetadata = attribute.value
+            returnData.description = rawMetadata.split("description")[1].split(":")[1].split('"')[1]
+            returnData.name = rawMetadata.split("name")[1].split('"')[1]
+            returnData.symbol = rawMetadata.split("symbol")[1].split('"')[1]
+            returnData.exponent = rawMetadata.split("exponent")[1].split(">")[0].split(":")[1].trim()
+            break
+          default:
+            break
+
+        }
+      }
+    } catch (err: any) {
+      error = err
+      status = Status.ERROR
+    }
+
+    return {
+      data: Object.keys(returnData).length === 0 ? null : returnData,
+      error,
+      status,
+    }
   }
 }
 
